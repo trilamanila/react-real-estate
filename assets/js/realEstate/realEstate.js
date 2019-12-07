d2e5e84d1a7e34b0396a43095e6469d39d7c83b9
Joncin Picol <trilamanila@gmail.com>
2019-12-07 17:34:34 -0500

    Created React Real Estate app, added header

---
+++ assets/js/realEstate/realEstate.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+import Header from './Header.js'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+  }
+  render () {
+    return (<div> 
+    <Header />
+     </div>)
+  }
+}
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<App />, app)
