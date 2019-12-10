ef46e884576627b4d735dcf121dede4efee9f652
Joncin Picol <trilamanila@gmail.com>
2019-12-10 01:29:39 -0500

    Styled pagination

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -186,10 +186,10 @@ export default class Header extends Component {
       </div>
     </section>
 
-    <section className="pagination">
-      <ul className="pagination-nums">
+    <section id="pagination">
+      <ul className="pages">
         <li>Prev</li>
-        <li>1</li>
+        <li className="active">1</li>
         <li>2</li>
         <li>3</li>
         <li>4</li>
--- public/js/components/realEstate.js
+++ public/js/components/realEstate.js
@@ -702,10 +702,10 @@ var Header = function (_Component) {
         ),
         _react2.default.createElement(
           'section',
-          { className: 'pagination' },
+          { id: 'pagination' },
           _react2.default.createElement(
             'ul',
-            { className: 'pagination-nums' },
+            { className: 'pages' },
             _react2.default.createElement(
               'li',
               null,
@@ -713,7 +713,7 @@ var Header = function (_Component) {
             ),
             _react2.default.createElement(
               'li',
-              null,
+              { className: 'active' },
               '1'
             ),
             _react2.default.createElement(
